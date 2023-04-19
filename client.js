ab502b4a6bae024be5e4cddebbed6d2524adbf11
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 00:15:37 +0900

    first commit

---
+++ client.js
@@ -0,0 +1,19 @@
+const net = require("net");
+// I am using Node's net library
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here
+    port: 50541// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  // conn.on("data", (data) => {
+  //   console.log(data);// code that does something when the connection is first established
+  // });
+  return conn;
+};
+
+module.exports = connect;
