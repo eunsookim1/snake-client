3bbcde173c6acb94481fdace98b17f302f9c6e23
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 01:45:08 +0900

    commented hard coded commands

--- client.js
+++ client.js
@@ -1,21 +1,30 @@
 const net = require("net");
 // I am using Node's net library
 
+
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "localhost", // IP address here
+    host: 'localhost', // IP address here
     port: 50541// PORT number here,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  // Connect event
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: ESK');
+    // setInterval(() => {
+    //   conn.write('Move: up');
+    // }, 500);
   });
+
+  // Data event
   
+ 
   return conn;
 };
 
-module.exports = connect;
+module.exports = { connect };
