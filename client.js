6e216cb31083d75ac69499c3c30ef46f0e0eb01a
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 00:38:00 +0900

    username aded

--- client.js
+++ client.js
@@ -10,9 +10,11 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  // conn.on("data", (data) => {
-  //   console.log(data);// code that does something when the connection is first established
-  // });
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: ESK');
+  });
+  
   return conn;
 };
 
