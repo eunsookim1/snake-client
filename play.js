34c1213dd4d5536a8506a54396a857eeaa7a7a7a
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 08:54:16 +0900

    added command keys

--- play.js
+++ play.js
@@ -1,8 +1,35 @@
-const connect = require("./client.js");
+const { connect } = require("./client.js");
 
 console.log("Connecting ...");
 const connection = connect();
+// console.log(connection);
 
-connection.on("data", (data) => {
-  console.log(data);// code that does something when the connection is first established
-});
+// connection.on("data", (data) => {
+//   console.log('abc', data);// code that does something when the connection is first established
+// });
+
+const handleUserInput = function(data) {
+  // console.log('data', data);
+  
+  if (data === 'w') {
+    connection.write('Move: up');
+  }
+  if (data === 'T') { // \u0003 maps to ctrl+c input
+    process.exit();
+  }
+};
+
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin; // process.stdin tells node from client
+  stdin.setRawMode(true); // when I type something in terminal for interpreting
+  stdin.setEncoding("utf8");
+  stdin.resume(); // socket to continue communication
+  stdin.on("data", (data) => {
+    handleUserInput(data);
+  });
+  return stdin;
+};
+
+// module.exports = setupInput;
+setupInput();
