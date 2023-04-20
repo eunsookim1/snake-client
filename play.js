1e6718ddbb06e6d6b13b038d2a71e19d6cab7e23
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 09:53:25 +0900

    dry coding

--- play.js
+++ play.js
@@ -1,35 +1,10 @@
 const { connect } = require("./client.js");
+const { setupInput } = require('./input');
 
 console.log("Connecting ...");
-const connection = connect();
-// console.log(connection);
 
-// connection.on("data", (data) => {
-//   console.log('abc', data);// code that does something when the connection is first established
-// });
 
-const handleUserInput = function(data) {
-  // console.log('data', data);
-  
-  if (data === 'w') {
-    connection.write('Move: up');
-  }
-  if (data === '\u0003') { // \u0003 maps to ctrl+c input
-    process.exit();
-  }
-};
+setupInput(connect());
 
-// setup interface to handle user input from stdin
-const setupInput = function() {
-  const stdin = process.stdin; // process.stdin tells node from client
-  stdin.setRawMode(true); // when I type something in terminal for interpreting
-  stdin.setEncoding("utf8");
-  stdin.resume(); // socket to continue communication
-  stdin.on("data", (data) => {
-    handleUserInput(data);
-  });
-  return stdin;
-};
-
-// module.exports = setupInput;
-setupInput();
+// const connection = connect();
+// setupInput(connection);
