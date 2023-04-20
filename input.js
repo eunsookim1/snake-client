4c69b18286fc9e6cfea3a396131c96e3d3064197
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 10:08:29 +0900

    say message input

--- input.js
+++ input.js
@@ -29,6 +29,9 @@ const handleUserInput = function(key) {
   if (key === 'a') {
     connection.write('Move: left');
   }
+  if (key === 'p') {
+    connection.write('Say: Great play!');
+  }
   if (key === '\u0003') { // \u0003 maps to ctrl+c input
     process.exit();
   }
