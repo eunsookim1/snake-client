f7dd87654af6cf9401f77a46a8c5088890f00130
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 15:15:55 +0900

    final changes

--- client.js
+++ client.js
@@ -5,8 +5,8 @@ const { IP, PORT } = require('./constants');
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: IP, // IP address here
-    port: PORT// PORT number here,
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
