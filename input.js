fce26a7f8807abeb19476ee8918272654c89348b
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 10:05:37 +0900

    key command change

--- input.js
+++ input.js
@@ -1,24 +1,5 @@
+// stores the active TCP connection object.
 let connection;
-
-const handleUserInput = function(data) {
-  // console.log('data', data);
-  
-  if (data === 'r') {
-    connection.write('Move: up');
-  }
-  if (data === 'f') {
-    connection.write('Move: down');
-  }
-  if (data === 'g') {
-    connection.write('Move: right');
-  }
-  if (data === 'd') {
-    connection.write('Move: left');
-  }
-  if (data === '\u0003') { // \u0003 maps to ctrl+c input
-    process.exit();
-  }
-};
 
 // setup interface to handle user input from stdin
 const setupInput = function(conn) { // belongs to play.js file
@@ -27,10 +8,30 @@ const setupInput = function(conn) { // belongs to play.js file
   stdin.setRawMode(true); // when I type something in terminal for interpreting
   stdin.setEncoding("utf8");
   stdin.resume(); // socket to continue communication
-  stdin.on("data", (data) => {
-    handleUserInput(data);
+  stdin.on("data", (key) => {
+    handleUserInput(key);
   });
   return stdin;
+};
+
+const handleUserInput = function(key) {
+  // console.log('data', data);
+  
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === '\u0003') { // \u0003 maps to ctrl+c input
+    process.exit();
+  }
 };
 
 module.exports = { setupInput };
