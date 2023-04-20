6b020cc0df0b5efff33964c63e0b79a58c22d963
eunsookim1 <92eunsoo@gmail.com>
2023-04-20 11:57:31 +0900

    format

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
-const net = require("net");
-// I am using Node's net library
+const net = require("net");   // I am using Node's net library
+const { IP, PORT } = require('./constants');
 
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: 'localhost', // IP address here
-    port: 50541// PORT number here,
+    host: IP, // IP address here
+    port: PORT// PORT number here,
   });
 
   // interpret incoming data as text
@@ -16,6 +16,7 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: ESK');
+    
     // setInterval(() => {
     //   conn.write('Move: up');
     // }, 500);
@@ -25,3 +26,6 @@ const connect = function() {
 };
 
 module.exports = { connect };
+
+// Message is broadcast to each connected client to inform a new player has joined
+//
